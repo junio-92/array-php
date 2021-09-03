2df89c85e7c3a4507b5da46610edda4cbb0f8700
junio-92 <9145junio@gmail.com>
2021-09-02 23:32:09 -0300

    vai

---
+++ atividade13divisao.php
@@ -0,0 +1,25 @@
+<?php
+
+function divisao($a1,$a2){
+    $dividir = $a1/$a2;
+  
+    return $dividir;
+    
+
+}
+echo divisao(50,2);
+
+
+echo "<br>";
+
+$oma = 4+2;
+
+echo "soma é $oma";
+
+
+if ($oma==6):
+    echo "bom";
+else:
+    echo "pessimo";
+endif;
+?>
---
+++ atividade13funcao.php
@@ -0,0 +1,46 @@
+<?php
+
+//funções variadas
+
+function exibircarro ($carro){
+    echo "O nome do carro é $carro";
+}
+exibircarro("Uno");
+
+
+echo "<br><br>";
+
+//função calculo
+
+
+function multiplicar($mult, $v1, $v2,$v3){
+
+$mult = 2*4*7;
+echo "O resultado da multiplicação é $mult";
+}
+
+multiplicar("resultado",4,6,8);
+
+echo "<br><br>";
+
+
+
+function divisao($d2,$d3){
+    $dividir=$d2/$d3;
+
+    return $dividir;
+}
+
+echo divisao(200,2);
+
+echo "<br><br>";
+
+// Array e foreach 
+
+$paises = array("Brasil", "Argentina","Mexio");
+foreach($paises as $nomes):
+    echo $nomes."<br>";
+endforeach;
+
+
+?>
